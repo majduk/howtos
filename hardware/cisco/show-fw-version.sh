266b3864742a5016d1b043e749391d05a5fb6682
Michał Ajduk <michal.ajduk@canonical.com>
2019-09-02 16:46:35 +0200

    Create show-fw-version.sh

---
+++ hardware/cisco/show-fw-version.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/expect -f
+
+set host [lindex $argv 0]
+set pass [lindex $argv 1]
+
+spawn ssh $host
+expect "password: "
+send "$pass\r"
+expect "# "
+send "show cimc/firmware\r"
+expect "# "
