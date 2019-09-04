7ec663f05c7c84651d9fb88da9e95a9d310543cf
Michał Ajduk <michal.ajduk@canonical.com>
2019-09-04 17:46:57 +0200

    Create reboot-idrac.sh

---
+++ hardware/dell/reboot-idrac.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/expect -f
+
+set host [lindex $argv 0]
+set pass [lindex $argv 1]
+set source [lindex $argv 2]
+set timeout 3600
+spawn ssh $host
+expect "password: "
+send "$pass\r"
+expect "> "
+send "racadm racreset soft\r"
+expect "> "
