12a5a0629524b00b8fae0fee0d162c737f02d093
Michał Ajduk <michal.ajduk@canonical.com>
2019-11-20 17:04:36 +0100

    Update relation.sh

--- juju/relation.sh
+++ juju/relation.sh
@@ -3,3 +3,9 @@
 ID=$( juju run --unit keystone/leader relation-ids ha )
 LIST=$( juju run --unit keystone/leader "relation-list -r  $ID " ) 
 juju run --unit keystone/leader "relation-get -r ha:52 - hacluster-keystone/0"
+
+
+juju run --unit keystone/leader "relation-get -r $ID - $LIST"
+
+#all:
+juju run --unit keystone/leader 'ID=$(relation-ids ha); for unit in $(relation-list -r  $ID); do relation-get -r $ID - $unit; done'
